b7203e0aabcb472fbbe6b4d72d1b102df44361e6
Luis caceres <131320337+caceresluis633@users.noreply.github.com>
2023-06-12 20:17:19 -0300

    Add files via upload

---
+++ ddl.sql
@@ -0,0 +1,38 @@
+drop database if exists petshop;
+create database petshop;
+use petshop;
+
+create table clientes (
+codigo int auto_increment primary key,
+nombre varchar(20) not null,
+apellido varchar(20) not null,
+cuit int(11),
+direccion varchar(20) not null
+);
+
+
+create table alimentos(
+codigo int auto_increment primary key,
+marca varchar(20),
+precio double,
+clasicas enum('DONCAN','GOURMET','NUTRICAN','NUTRIQUE','VITALCAT'),
+codigocliente int not null,
+constraint FK_clientes_codcli
+foreign key (codigocliente)
+references clientes(codigo)
+on delete cascade
+);
+
+create table facturas(
+letra char(1),
+numero int auto_increment primary key,
+monto double,
+fecha date,
+codigocliente int not null,
+codigoalimento int not null,
+constraint FK_facturas_codcli
+foreign key (codigocliente)
+references clientes(codigo)
+on delete cascade
+
+);
